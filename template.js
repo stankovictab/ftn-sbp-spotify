cd219e8c6b4dc5749d699b5c0a953d5dc0d84342
stankovictab <stankovictab@gmail.com>
2022-05-30 11:36:35 +0200

    new: template.js addon

--- template.js
+++ template.js
@@ -15,3 +15,86 @@ audio_feature = {
 	"valence": 0.7870000004768372
 };
 
+// id,disc_number,duration,explicit,audio_feature_id,name,preview_url,track_number,popularity,is_playable
+// 1dizvxctg9dHEyaYTFufVi,1,275893,1,1dizvxctg9dHEyaYTFufVi,Gz And Hustlas (feat. Nancy Fletcher),,12,0,
+
+track = {
+	"id": "1dizvxctg9dHEyaYTFufVi",
+	"disc_number": 1,
+	"duration": 275893,
+	"explicit": 1,
+	"audio_feature_id": "1dizvxctg9dHEyaYTFufVi",
+	"name": "Gz And Hustlas (feat. Nancy Fletcher)",
+	"preview_url": "",
+	"track_number": 12,
+	"popularity": 0,
+	"is_playable": ""
+};
+
+// r_track_artist
+// track_id,artist_id
+// 2jKoVlU7VAmExKJ1Jh3w9P,4tujQJicOnuZRLiBFdp3Ou
+
+r_track_artist = {
+	"track_id": "2jKoVlU7VAmExKJ1Jh3w9P",
+	"artist_id": "4tujQJicOnuZRLiBFdp3Ou"
+};
+
+// r_artist_genre
+// genre_id,artist_id
+// detroit hip hop,4tujQJicOnuZRLiBFdp3Ou
+
+r_artist_genre = {
+	"genre_id": "detroit hip hop",
+	"artist_id": "4tujQJicOnuZRLiBFdp3Ou"
+};
+
+// r_albums_tracks
+// album_id,track_id
+// 6os2Mv58OYnQClPf7B9E1s,3HnrHGLE9u2MjHtdobfWl9
+
+r_albums_tracks = {
+	"album_id": "6os2Mv58OYnQClPf7B9E1s",
+	"track_id": "3HnrHGLE9u2MjHtdobfWl9"
+};
+
+// r_albums_artist
+// album_id,artist_id
+// 6os2Mv58OYnQClPf7B9E1s,2HS2wQTJXpA65XWOKlAVxk
+
+r_albums_artist = {
+	"album_id": "6os2Mv58OYnQClPf7B9E1s",
+	"artist_id": "2HS2wQTJXpA65XWOKlAVxk"
+};
+
+// genres
+// id
+// detroit hip hop
+
+genre = {
+	"id": "detroit hip hop"
+};
+
+// artists
+// name,id,popularity,followers
+// Xzibit,4tujQJicOnuZRLiBFdp3Ou,69,1193665
+
+artist = {
+	"name": "Xzibit",
+	"id": "4tujQJicOnuZRLiBFdp3Ou",
+	"popularity": 69,
+	"followers": 1193665
+};
+
+// albums
+// id,name,album_group,album_type,release_date,popularity
+// 2jKoVlU7VAmExKJ1Jh3w9P,"Alkaholik (feat. Erik Sermon, J Ro & Tash)",,album,954633600000,0
+
+album = {
+	"id": "2jKoVlU7VAmExKJ1Jh3w9P",
+	"name": "Alkaholik (feat. Erik Sermon, J Ro & Tash)",
+	"album_group": "",
+	"album_type": "album",
+	"release_date": "954633600000",
+	"popularity": 0
+};
