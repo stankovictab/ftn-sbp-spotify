84f19d03bc15a3245ee3cac0f4af0e9997684ccf
stankovictab <stankovictab@gmail.com>
2022-06-09 13:47:01 +0200

    optimized exporting csv

--- app.py
+++ app.py
@@ -9,23 +9,22 @@ client = pymongo.MongoClient("mongodb://localhost:27017/", username="root", pass
 
 db = client["spotify"] # Creating db in mongo
 
-tables = ["audio_features", "tracks", "albums", "artists", "genres", "r_albums_artists", "r_albums_tracks", "r_artist_genre", "r_track_artist"]
+tables = ["albums", "artists", "audio_features", "tracks", "genres", "r_albums_artists", "r_albums_tracks", "r_artist_genre", "r_track_artist"]
 
-print("Reading CSV...")
-df = pd.read_csv("data/audio_features.csv")
-df.drop('analysis_url', axis=1, inplace=True)
-low = 0
-high = 2000000
-for i in range(math.ceil(len(df) / 2000000)):
-	print(f"I: {i}")
-	df_sub = df[low:high]
-	print(df_sub.head())
-	low += 2000000
-	if high == 8000000:
-		high = len(df)
-	else:
-		high += 2000000
-	df_sub.to_csv(f"data/audio_features_decompose/audio_features_{i}.csv", index=False)
+# print("Reading CSV...")
+# df = pd.read_csv("data/audio_features.csv")
+# low = 0
+# high = 2000000
+# for i in range(math.ceil(len(df) / 2000000)):
+# 	print(f"I: {i}")
+# 	df_sub = df[low:high]
+# 	print(df_sub.head())
+# 	low += 2000000
+# 	if high == 8000000:
+# 		high = len(df)
+# 	else:
+# 		high += 2000000
+# 	df_sub.to_csv(f"data/audio_features_decompose/audio_features_{i}.csv", index=False)
 
 # TODO: Decompose other csv files
 
--- data/extraction.py
+++ data/extraction.py
@@ -9,31 +9,40 @@ cursor = conn.cursor()
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 print("Database description: \n", cursor.fetchall())
 
-db_df = pd.read_sql_query("SELECT * FROM albums", conn)
+print("Exporting 'albums' into CSV...")
+db_df = pd.read_sql_query("SELECT id, name, album_type, release_date, popularity FROM albums", conn)
 db_df.to_csv('albums.csv', index=False)
 
+print("Exporting 'artists' into CSV...")
 db_df = pd.read_sql_query("SELECT * FROM artists", conn)
 db_df.to_csv('artists.csv', index=False)
 
-db_df = pd.read_sql_query("SELECT * FROM audio_features", conn)
+print("Exporting 'audio_features' into CSV...")
+db_df = pd.read_sql_query("SELECT id, round(acousticness,4) as acousticness, round(danceability,4) as danceability, duration, round(energy,4) as energy, round(instrumentalness,4) as instrumentalness, key, round(liveness,4) as liveness, round(loudness,4) as loudness, mode, round(speechiness,4) as speechiness, round(tempo,4) as tempo, time_signature, round(valence,4) as valence FROM audio_features", conn)
 db_df.to_csv('audio_features.csv', index=False)
 
+print("Exporting 'genres' into CSV...")
 db_df = pd.read_sql_query("SELECT * FROM genres", conn)
 db_df.to_csv('genres.csv', index=False)
 
+print("Exporting 'r_albums_artists' into CSV...")
 db_df = pd.read_sql_query("SELECT * FROM r_albums_artists", conn)
 db_df.to_csv('r_albums_artists.csv', index=False)
 
+print("Exporting 'r_albums_tracks' into CSV...")
 db_df = pd.read_sql_query("SELECT * FROM r_albums_tracks", conn)
 db_df.to_csv('r_albums_tracks.csv', index=False)
 
+print("Exporting 'r_artist_genre' into CSV...")
 db_df = pd.read_sql_query("SELECT * FROM r_artist_genre", conn)
 db_df.to_csv('r_artist_genre.csv', index=False)
 
+print("Exporting 'r_track_artist' into CSV...")
 db_df = pd.read_sql_query("SELECT * FROM r_track_artist", conn)
 db_df.to_csv('r_track_artist.csv', index=False)
 
-db_df = pd.read_sql_query("SELECT * FROM tracks", conn)
+print("Exporting 'tracks' into CSV...")
+db_df = pd.read_sql_query("SELECT id, duration, explicit, audio_feature_id, name, popularity FROM tracks", conn)
 db_df.to_csv('tracks.csv', index=False)
 
 # cursor = conn.cursor()
