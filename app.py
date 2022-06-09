477764f2b423726883ea67d0ade1ca5ba19bb78c
stankovictab <stankovictab@gmail.com>
2022-06-09 20:10:07 +0200

    extraction finished

--- app.py
+++ app.py
@@ -1,39 +1,7 @@
 import pymongo
 import pandas as pd
-import json
-import math
 
-# mongodb://mongo:27017 doesn't work
-# Authentication is also needed
 client = pymongo.MongoClient("mongodb://localhost:27017/", username="root", password="example")
-
 db = client["spotify"] # Creating db in mongo
 
-tables = ["albums", "artists", "audio_features", "tracks", "genres", "r_albums_artists", "r_albums_tracks", "r_artist_genre", "r_track_artist"]
-
-# print("Reading CSV...")
-# df = pd.read_csv("data/audio_features.csv")
-# low = 0
-# high = 2000000
-# for i in range(math.ceil(len(df) / 2000000)):
-# 	print(f"I: {i}")
-# 	df_sub = df[low:high]
-# 	print(df_sub.head())
-# 	low += 2000000
-# 	if high == 8000000:
-# 		high = len(df)
-# 	else:
-# 		high += 2000000
-# 	df_sub.to_csv(f"data/audio_features_decompose/audio_features_{i}.csv", index=False)
-
-# TODO: Decompose other csv files
-
-
-# for table in tables:
-# 	print(f"Importing table '{table}'...")
-# 	print("Reading CSV...")
-# 	df = pd.read_csv(f"data/{table}.csv")
-# 	print("Turning into dictionary...")
-# 	data = df.to_dict(orient='records')
-# 	print(f"Inserting into '{table}' collection...")
-# 	db[f"{table}"].insert_many(data)
+print(db["albums"].database.name)
--- data/extraction.py
+++ data/extraction.py
@@ -1,52 +1,53 @@
 import sqlite3
 import pandas as pd
+import pymongo
+import math
 
-conn = sqlite3.connect("spotify.sqlite", isolation_level=None,
-                       detect_types=sqlite3.PARSE_COLNAMES)
+CHUNK_SIZE = 1000000
+
+client = pymongo.MongoClient("mongodb://localhost:27017/", username="root", password="example")
+db = client["spotify"] # Creating db in mongo
+
+conn = sqlite3.connect("spotify.sqlite", isolation_level=None, detect_types=sqlite3.PARSE_COLNAMES)
 conn.text_factory = lambda b: b.decode(errors = 'ignore')
 
 cursor = conn.cursor()
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 print("Database description: \n", cursor.fetchall())
 
-print("Exporting 'albums' into CSV...")
-db_df = pd.read_sql_query("SELECT id, name, album_type, release_date, popularity FROM albums", conn)
-db_df.to_csv('albums.csv', index=False)
+def zbrcniGa(table, query):
+	print(f"Working on '{table}'...")
+	db_df = pd.read_sql_query(f"{query}", conn)
+	# db_df.to_csv(f'{table}.csv', index=False)
+	print("Turning into dictionary...")
 
-print("Exporting 'artists' into CSV...")
-db_df = pd.read_sql_query("SELECT * FROM artists", conn)
-db_df.to_csv('artists.csv', index=False)
+	low = 0
+	high = CHUNK_SIZE
+	for i in range(math.ceil(len(db_df) / CHUNK_SIZE)):
+		print(f"i: {i}")
+		db_df_sub = db_df[low:high]
+		print(db_df_sub.head())
+		low += CHUNK_SIZE
+		high += CHUNK_SIZE
+		if high > len(db_df):
+			high = len(db_df)
+		# df_sub.to_csv(f"data/{table}_decompose/audio_features_{i}.csv", index=False)
+		data = db_df_sub.to_dict(orient='records')
+		print(f"Inserting into '{table}' collection...")
+		db[f"{table}"].insert_many(data)
+		del(db_df_sub)
+		del(data)
+	del(db_df)
+	print("-----------------------------------------")
 
-print("Exporting 'audio_features' into CSV...")
-db_df = pd.read_sql_query("SELECT id, round(acousticness,4) as acousticness, round(danceability,4) as danceability, duration, round(energy,4) as energy, round(instrumentalness,4) as instrumentalness, key, round(liveness,4) as liveness, round(loudness,4) as loudness, mode, round(speechiness,4) as speechiness, round(tempo,4) as tempo, time_signature, round(valence,4) as valence FROM audio_features", conn)
-db_df.to_csv('audio_features.csv', index=False)
-
-print("Exporting 'genres' into CSV...")
-db_df = pd.read_sql_query("SELECT * FROM genres", conn)
-db_df.to_csv('genres.csv', index=False)
-
-print("Exporting 'r_albums_artists' into CSV...")
-db_df = pd.read_sql_query("SELECT * FROM r_albums_artists", conn)
-db_df.to_csv('r_albums_artists.csv', index=False)
-
-print("Exporting 'r_albums_tracks' into CSV...")
-db_df = pd.read_sql_query("SELECT * FROM r_albums_tracks", conn)
-db_df.to_csv('r_albums_tracks.csv', index=False)
-
-print("Exporting 'r_artist_genre' into CSV...")
-db_df = pd.read_sql_query("SELECT * FROM r_artist_genre", conn)
-db_df.to_csv('r_artist_genre.csv', index=False)
-
-print("Exporting 'r_track_artist' into CSV...")
-db_df = pd.read_sql_query("SELECT * FROM r_track_artist", conn)
-db_df.to_csv('r_track_artist.csv', index=False)
-
-print("Exporting 'tracks' into CSV...")
-db_df = pd.read_sql_query("SELECT id, duration, explicit, audio_feature_id, name, popularity FROM tracks", conn)
-db_df.to_csv('tracks.csv', index=False)
-
-# cursor = conn.cursor()
-# for row in cursor.execute("SELECT * FROM albums"):
-# 	print(row)
+zbrcniGa("albums", "SELECT id, name, album_type, release_date, popularity FROM albums")
+zbrcniGa("artists", "SELECT * FROM artists")
+zbrcniGa("audio_features", "SELECT id, round(acousticness,4) as acousticness, round(danceability,4) as danceability, duration, round(energy,4) as energy, round(instrumentalness,4) as instrumentalness, key, round(liveness,4) as liveness, round(loudness,4) as loudness, mode, round(speechiness,4) as speechiness, round(tempo,4) as tempo, time_signature, round(valence,4) as valence FROM audio_features")
+zbrcniGa("genres", "SELECT * FROM genres")
+zbrcniGa("r_albums_artists", "SELECT * FROM r_albums_artists")
+zbrcniGa("r_albums_tracks", "SELECT * FROM r_albums_tracks")
+zbrcniGa("r_artist_genre", "SELECT * FROM r_artist_genre")
+zbrcniGa("r_track_artist", "SELECT * FROM r_track_artist")
+zbrcniGa("tracks", "SELECT id, duration, explicit, audio_feature_id, name, popularity FROM tracks")
 
 conn.close()
